0571166f94f67a8dfc2bac4137c7673192b999c5
kgc42 <kgc42@cornell.edu>
2025-05-19 15:55:30 -0400

    allowed for multipel files

--- prelim-calendar/frontend/src/webpage.tsx
+++ prelim-calendar/frontend/src/webpage.tsx
@@ -3,13 +3,22 @@ import DropBox from './components/dropbox'
 import './webpage.css'
 
 function Webpage() {
-  const [file, setFile] = useState<File | null>(null)
+  const [files, setFiles] = useState<File[]>([])
 
   return (
     <div className="App">
       <h1>Drop your Syllabuses Below</h1>
-      <DropBox onDrop={(files) => setFile(files[0])} />
-      {file && <p className="mt-4 text-center">Selected: {file.name}</p>}
+      <DropBox onDrop={(newFile) => setFiles(existingFiles => [...existingFiles, ...newFile])} />
+      {files.length > 0 && (
+        <div className="mt-4 text-center">
+          <p>Selected files:</p>
+          <ul>
+            {files.map((file, idx) => (
+              <li key={idx}>{file.name}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   )
 }
